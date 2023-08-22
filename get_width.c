c777bd516a92f168682fbb6329ec4cd6e72fa8d5
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-08-22 16:43:26 -0700

    Printf

--- get_width.c
+++ get_width.c
@@ -13,7 +13,7 @@ int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
 	int width = 0;
-       
+
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
@@ -21,7 +21,7 @@ int get_width(const char *format, int *i, va_list list)
 			width *= 10;
 			width += format[curr_i] - '0';
 		}
-		
+
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
@@ -32,6 +32,6 @@ int get_width(const char *format, int *i, va_list list)
 			break;
 	}
 	*i = curr_i - 1;
-	
+
 	return (width);
 }
